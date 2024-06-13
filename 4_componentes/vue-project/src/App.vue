87ee531718dce25b02f715c9cc94e4afc49d39bb
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-13 11:07:56 -0300

    aprendendo a utilizar componentes

---
+++ 4_componentes/vue-project/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <primeiroComponente/>
+</template>
+
+<script>
+import primeiroComponente from './components/primeiroComponente.vue'
+export default {
+  name: 'app',
+  components: {
+    primeiroComponente
+  }
+}
+</script>
---
+++ 4_componentes/vue-project/src/components/primeiroComponente.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>olá Vue!</h1>
+</template>
+
+<script>
+export default {
+    name: 'primeiroComponente'
+}
+</script>
