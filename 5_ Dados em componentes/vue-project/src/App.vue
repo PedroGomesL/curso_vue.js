381a5eac920e882cc7f8b4d53c8622683d62d3ff
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-13 12:06:31 -0300

    dados em componentes

---
+++ 5_ Dados em componentes/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <primeiroComponente/>
+  <p>o dado de app é: {{ testando }}</p>
+</template>
+
+<script>
+import primeiroComponente from './components/primeiroComponente.vue'
+export default {
+  name: 'app',
+  components: {
+    primeiroComponente
+  },
+  data() {
+    return {
+      testando: "teste"
+    }
+  }
+}
+</script>
