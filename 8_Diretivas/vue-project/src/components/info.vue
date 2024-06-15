d09e9689eecf59bf18276c055fa2e0596676add3
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2024-06-15 12:42:19 -0300

    diretivas em Vue.JS, v-if e v-show

---
+++ 8_Diretivas/vue-project/src/components/info.vue
@@ -0,0 +1,24 @@
+<template>
+<p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+<p v-else>Estou em busca de novas oportunidades</p>
+<p>Utilizo as seguintes tecnologias:</p>
+<ul>
+    <li>JS</li>
+    <li>PHP</li>
+    <li>Python</li>
+</ul>
+<p v-show="mostrar_email">mande uma mensagem para {{ email }}</p>
+</template>
+
+<script>
+    export default {
+        name: 'Info',
+        data() {
+            return {
+                esta_trabalhando: false,
+                mostrar_email: false,
+                email: 'pedron@gmail.com'
+            }
+        }
+    }
+</script>
