6b6de13e327d90cfde60da364ea0cfb0194ed533
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2024-06-15 10:05:49 -0300

    Hierarquia de Componenetes

---
+++ 7_Hierarquia_de_Componentes/Hierarquia de componentes/src/components/info.vue
@@ -0,0 +1,15 @@
+<template>
+<p>Estou trabalhando no momento</p>
+<p>Utilizo as seguintes tecnologias:</p>
+<ul>
+    <li>JS</li>
+    <li>PHP</li>
+    <li>Python</li>
+</ul>
+</template>
+
+<script>
+    export default {
+        name: 'Info'
+    }
+</script>
