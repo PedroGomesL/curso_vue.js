d66bc81477986def4fc1892f5023cf2ac73ecbdf
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2024-06-16 13:41:12 -0300

    CSS SCOPED

---
+++ 11_CSS_global_scoped/vue-project/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <primeiroComponente/>
+  <p>o dado de app é: {{ testando }}</p>
+   <LifeCycle/>
+</template>
+
+<script>
+import primeiroComponente from './components/primeiroComponente.vue'
+import LifeCycle from './components/lifeCycle.vue'
+export default {
+  name: 'app',
+  components: {
+    primeiroComponente,
+    LifeCycle
+  },
+  data() {
+    return {
+      testando: "teste"
+    }
+  }
+}
+</script>
---
+++ 11_CSS_global_scoped/vue-project/src/components/info.vue
@@ -0,0 +1,51 @@
+<template>
+<p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+<p v-else>Estou em busca de novas oportunidades</p>
+<p>Utilizo as seguintes tecnologias:</p>
+<ul>
+    <li>JS</li>
+    <li>PHP</li>
+    <li>Python</li>
+</ul>
+<div>
+    <button @click="showEmail">{{ textoBotao }}</button>
+</div>
+<p v-show="mostrar_email">mande uma mensagem para {{ email }}</p>
+<p class="teste" >Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+<Picture />
+</template>
+
+<script>
+import Picture from './picture.vue'
+    export default {
+        name: 'Info',
+        components: {
+            Picture
+        },
+        data() {
+            return {
+                esta_trabalhando: false,
+                mostrar_email: false,
+                email: 'pedron@gmail.com',
+                meu_link: 'https://google.com',
+                textoBotao: 'Mostrar e-mail'
+            }
+        },
+        methods: {
+            showEmail() {
+                this.mostrar_email = !this.mostrar_email
+                if(!this.mostrar_email) {
+                    thi.textoBotao = 'Mostras e-mail'
+                } else {
+                    this.textoBotao = 'Esconder e-mail'
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .paragrafo-pai {
+        background-color: red;
+    }
+</style>
