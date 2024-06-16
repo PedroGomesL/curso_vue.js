99b026cd4ade276338063965ad67367679230c3a
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2024-06-16 15:09:44 -0300

    v-for

---
+++ 12_Renderização_de_Lista/vue-project/src/components/info.vue
@@ -0,0 +1,61 @@
+<template>
+<p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+<p v-else>Estou em busca de novas oportunidades</p>
+<p>Utilizo as seguintes tecnologias:</p>
+<ul>
+    <li v-for="(tecnology, index) in backend_tecnologies" v-bind:key="index">{{tecnology}}</li>
+</ul>
+<p>Utilizado as seguintes tecnologias para front-end: </p>
+<ul>
+    <li v-for="tecnology in frontend_techologies" :key="tecnology.id">
+        {{ tecnology.language }}
+    </li>
+</ul>
+<div>
+    <button @click="showEmail">{{ textoBotao }}</button>
+</div>
+<p v-show="mostrar_email">mande uma mensagem para {{ email }}</p>
+<p class="teste" >Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+<Picture />
+</template>
+
+<script>
+import Picture from './picture.vue'
+    export default {
+        name: 'Info',
+        components: {
+            Picture
+        },
+        data() {
+            return {
+                esta_trabalhando: false,
+                mostrar_email: false,
+                email: 'pedron@gmail.com',
+                meu_link: 'https://google.com',
+                textoBotao: 'Mostrar e-mail',
+                backend_tecnologies: ['JS', 'PHP', 'Python'],
+                frontend_techologies: [
+                    {id: 1, language: 'HTML'},
+                    {id: 2, language: 'CSS'},
+                    {id: 3, language: 'Vue'}
+                ]
+            }
+        },
+        methods: {
+            showEmail() {
+                this.mostrar_email = !this.mostrar_email
+                if(!this.mostrar_email) {
+                    thi.textoBotao = 'Mostras e-mail'
+                } else {
+                    this.textoBotao = 'Esconder e-mail'
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .paragrafo-pai {
+        background-color: red;
+    }
+</style>
---
+++ 12_Renderização_de_Lista/vue-project/src/components/pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+<h2>Está é a descrição da pessoa: </h2>
+<Info/>
+<Form/>
+</template>
+
+<script>
+import Info from './info.vue'
+import Form from './Form/form.vue'
+export default {
+    name: 'Pessoa',
+    components: {
+        Info,
+        Form
+    },
+    data() {
+        return {
+            nome: 'Pedro'
+        }
+    }
+}
+</script>
