dfb4cff29c49c5d933817fad83c16e62a1c571e9
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-17 15:09:54 -0300

    multiplos eventos

---
+++ 14_Multiplos_eventos/vue-project/src/components/primeiroComponente.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>olá Vue!</h1>
+    <p>meu nome é {{ nome }}, e trabalho com {{ profissão }}</p>
+    <MultiplosEventos />
+</template>
+
+<script>
+
+import MultiplosEventos from './MultiplosEventos.vue'
+export default {
+    name: 'primeiroComponente',
+    components: {
+        MultiplosEventos
+    },
+    data() {
+        return {
+            nome: "Pedro",
+            profissão: "Agiota"
+        }
+    }
+}
+</script>
