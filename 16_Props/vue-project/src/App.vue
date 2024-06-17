eeaf76d0d49efa0a8619396beaa4b90cf0012ab1
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-17 16:37:07 -0300

    upando Props

---
+++ 16_Props/vue-project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Header :esta_logado="true" />
+  <primeiroComponente />
+  <p class="paragrafo-pai">o dado de app é: {{ testando }}</p>
+  <LifeCycle />
+  <pessoa />
+
+</template>
+
+<script>
+import primeiroComponente from './components/primeiroComponente.vue'
+import LifeCycle from './components/lifeCycle.vue'
+import Pessoa from './components/pessoa.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    primeiroComponente,
+    LifeCycle,
+    Pessoa,
+    Header
+  },
+  data() {
+    return {
+      testando: "teste"
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #3d3d3d;
+  color: white;
+}
+
+a {
+  color: red;
+}
+
+.teste {
+  background-color: red;
+}
+</style>
