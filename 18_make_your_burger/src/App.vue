72782c3956fbfdd8f6d3e214059018890b1f8e2e
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-18 15:15:02 -0300

    finalizando o nav bar e o footer

--- 18_make_your_burger/src/App.vue
+++ 18_make_your_burger/src/App.vue
@@ -1,18 +1,44 @@
 <template>
   <div>
-    <navBar />
+    <navBar :logo="logo_src" :alt="app_name" />
+    <router-view />
     <Footer />
-    <router-view/>
   </div>
 </template>
 
 <script>
-  import navBar from "./components/navBar.vue"
-  import Footer from "./components/footer.vue"
-  export default {
-    components: {
-      navBar,
-      Footer,
+import navBar from "./components/navBar.vue"
+import Footer from "./components/footer.vue"
+export default {
+  components: {
+    navBar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make your Burger"
     }
   }
+}
 </script>
+
+<style>
+* {
+  font-family: helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom:30px ;
+    color: #222;
+  }
+}
+</style>
