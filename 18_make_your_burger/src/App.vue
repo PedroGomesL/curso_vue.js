aa3129976d93c88c22b8dc2c20992522808c58bc
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-18 14:38:40 -0300

    definando a rota pedido e home

--- 18_make_your_burger/src/App.vue
+++ 18_make_your_burger/src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <navBar />
+    <Footer />
+    <router-view/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  import navBar from "./components/navBar.vue"
+  import Footer from "./components/footer.vue"
+  export default {
+    components: {
+      navBar,
+      Footer,
+    }
+  }
+</script>
