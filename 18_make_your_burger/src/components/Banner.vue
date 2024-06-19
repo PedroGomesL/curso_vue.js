9c19e32cd74c49b17d7529f07b9321c32544dbd7
Pedro H. Gomes <pedrohenriquegomeslima1@gmail.com>
2024-06-19 12:53:01 -0300

    upando o Banner finalizado utilizando EM para adaptar

---
+++ 18_make_your_burger/src/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Burger"
+
+}
+</script>
+
+<style scoped>
+
+#main-banner {
+    background-image: url("../../public/img/burger.jpg");
+    background-position: center;
+    background-size:cover;
+    height: 30em;
+    display:flex;
+    background-repeat: no-repeat;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+#main-banner h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 2.5em;
+    background-color: #222;
+    padding: 2%;
+}
+</style>
